5010a1a8bfc33834a7530432e8cc658bdc0e48ab
ARNO-Dupril <134289859+ARNO-Dupril@users.noreply.github.com>
2024-07-27 05:58:25 +0100

    frontend -1.0.1

--- cm_le_printemps_frontend/src/app/shared/navbar/navbar.component.ts
+++ cm_le_printemps_frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  
+  constructor(){}
+
+  menuVariable:boolean = false;
+  xlg:boolean = this.menuVariable;
+
+  openMenu(){
+    console.log("old: ", this.menuVariable);
+    this.menuVariable = !this.menuVariable;
+    this.xlg = this.menuVariable;
+  }
 }
